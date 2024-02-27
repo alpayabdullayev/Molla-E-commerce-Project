a35e2c79d981a9647f1a8b7f0c7aa53753d5b392
alpayabdullayev <alpayabdullayev0@gmail.com>
2024-02-27 14:12:01 +0400

    used nurana

---
+++ MollaContext/src/components/PageTitle/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./index.scss"
+
+const PageTitle = () => {
+  return (
+    <>
+    <div className="sectionTitleBanner">
+
+    </div>
+    </>
+  )
+}
+
+export default PageTitle
